bc9b2194ca6da2915b629fde967155f97732636a
Joshua Davidson <email@jwaynedavidson.com>
2022-04-12 23:27:00 -0500

    move default gimbals into repo

---
+++ packages/main/renderer/types.ts
@@ -0,0 +1,46 @@
+/* eslint-disable no-unused-vars */
+export interface StickAxisInfo {
+    min: number,
+    max: number,
+    increment: number,
+}
+
+export interface StickManifestInfo {
+    name: string,
+    frames: {
+        location: string,
+        fileNameFormat: string,
+        x: StickAxisInfo,
+        y: StickAxisInfo,
+    }
+}
+
+export interface StickPositions {
+    roll: number,
+    pitch: number,
+    yaw: number,
+    throttle: number,
+}
+
+export interface LogEntry extends StickPositions {
+    time: number,
+}
+
+export interface FramePaths {
+    leftFramePath: string,
+    rightFramePath: string,
+}
+
+export interface DemuxFilePair {
+    leftDemuxFilePath: string,
+    rightDemuxFilePath: string,
+}
+
+// See: https://github.com/typescript-eslint/typescript-eslint/issues/325
+// eslint-disable-next-line no-shadow
+export enum TransmitterModes {
+    Mode1 = 1,
+    Mode2,
+    Mode3,
+    Mode4,
+}
