4f6045043ae366811f6aacf16c6b216799083a53
William Etheredge <me@wetheredge.com>
2022-09-23 02:01:00 -0500

    Only AppImage & bundle ffmpeg for linux

--- packages/main/utils.ts
+++ packages/main/utils.ts
@@ -11,7 +11,7 @@ export function getAsarUnpackedPath(asarPath: string): string {
     return asarPath;
 }
 
-// Function to get the name of an executable for the current platform
+// Get the name of an executable for the current platform
 export function getExecutableName(executable: string): string {
     const extension = process.platform === 'win32' ? '.exe' : '';
     return executable + extension;
@@ -20,13 +20,9 @@ export function getExecutableName(executable: string): string {
 // Get the executable name for a tool. For vendored tools, returns the
 // absolute path. Otherwise, returns the name of the system executable.
 export function getToolName(tool: 'blackbox_decode' | 'ffmpeg'): string {
-    if (tool === 'ffmpeg' && process.platform === 'linux') {
-        return 'ffmpeg';
-    }
-
     const vendorDir = tool === 'blackbox_decode' ? 'blackbox-tools-0.4.3' : tool;
     const exeName = getExecutableName(tool);
-    const absoluteDir = path.resolve(__dirname, './vendor/', vendorDir, exeName);
+    const absolute = path.resolve(__dirname, './vendor/', vendorDir, exeName);
 
-    return getAsarUnpackedPath(absoluteDir);
+    return getAsarUnpackedPath(absolute);
 }
